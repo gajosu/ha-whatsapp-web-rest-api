544874bd64dcb50156163d225b38db1d4cbf0479
Gabriel Gonzalez <gajosu15@gmail.com>
2022-11-10 18:33:31 -0500

    fix tests

--- tests/Libs/EventBus.test.ts
+++ tests/Libs/EventBus.test.ts
@@ -3,17 +3,24 @@ import { EventBus } from '../../src/Libs/EventBus';
 
 describe('Event Bus test', () => {
     it('get event', () => {
+        let count = 0;
 
-        expect(true).toBe(true)
-        EventBus.getInstance().register('hello-world', (name: string) => {
+        const registry = EventBus.getInstance().register('hello-world', (name: string) => {
             if (name) {
                 expect(name).toBe('Luis');
-            }else{
+            } else {
                 expect(name).toBe(undefined);
             }
+
+            count++;
         });
 
         EventBus.getInstance().dispatch<string>('hello-world', 'Luis');
         EventBus.getInstance().dispatch<string>('hello-world');
+
+        registry.unregister();
+        EventBus.getInstance().dispatch<string>('hello-world');
+
+        expect(count).toBe(2);
     })
 })
