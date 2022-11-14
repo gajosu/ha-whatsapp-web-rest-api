70b2056482078afe9d180b00486e9a8ad00620b3
Gabriel Gonzalez <gajosu15@gmail.com>
2022-11-13 23:54:55 -0500

    add websocket and webserver to global controller

--- src/Libs/Controller.ts
+++ src/Libs/Controller.ts
@@ -1,17 +1,24 @@
 
 import Logger from './Logger';
+import WebServer from './WebServer';
+import WebSocket from './WebSocket';
 import Whatsapp from './Whatsapp';
 
 export default class Controller {
 
     private logger: Logger = new Logger();
     constructor(
-        private whatsapp: Whatsapp
+        private whatsapp: Whatsapp,
+        private webserver: WebServer,
+        private websocket: WebSocket
     ) {
     }
 
     public start() {
         this.logger.info('Starting Controller');
         this.whatsapp.start();
+
+        this.webserver.start();
+        this.websocket.start();
     }
 }
