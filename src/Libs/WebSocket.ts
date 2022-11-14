753a37f3359bde781d1b3f9e43f27109554ee920
Gabriel Gonzalez <gajosu15@gmail.com>
2022-11-13 23:54:34 -0500

    add websocket controller

---
+++ src/Libs/WebSocket.ts
@@ -0,0 +1,42 @@
+import { Server, Socket } from "socket.io"
+import http from 'http'
+import Logger from "./Logger";
+import { EventBus } from './EventBus';
+
+export default class WebSocket {
+
+    private io: Server;
+    private logger = new Logger('WebSocket', 'blue');
+
+    constructor(
+        private httpServer: http.Server,
+    ) {
+
+    }
+
+    public start() {
+        this.logger.info("Starting WebSocket");
+        this.io = new Server(this.httpServer);
+        this.io.on("connection", this.onConnection.bind(this));
+        this.io.on("disconnect", this.onDisconnect.bind(this));
+    }
+
+    public stop() {
+        this.logger.info("Stopping WebSocket");
+        this.io.close();
+    }
+
+    private onConnection(socket: Socket) {
+        EventBus.getInstance().dispatch("socket.connection", socket);
+        this.logger.info("New connection from " + socket.id);
+
+        EventBus.getInstance().register("whatsapp.qr", (qr: string) => {
+            socket.emit("qr_code",  { data: qr });
+        });
+    }
+
+    public onDisconnect(socket: Socket) {
+        EventBus.getInstance().dispatch("socket.disconnect", socket);
+        this.logger.info("Disconnect from " + socket.id);
+    }
+}
