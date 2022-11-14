d03754240fb55a8b54740fa1220675fc7cc3a89f
Gabriel Gonzalez <gajosu15@gmail.com>
2022-11-14 18:24:33 -0500

    add console args to global config

--- src/config/GlobalConfig.ts
+++ src/config/GlobalConfig.ts
@@ -1,7 +1,16 @@
-const config:any = {
-    port: 3000,
+import { parse } from 'ts-command-line-args';
+interface IConfigArguments {
+    port: number;
+    supervisorToken: string|undefined;
+    browserPath: string|undefined;
 }
 
-export default function getConfig(name: string, defaultValue:any = null): any {
+const config:any = parse<IConfigArguments>({
+    port: { type: Number, alias: 'p', defaultValue: 3000, optional: true },
+    supervisorToken: { type: String, alias: 't', defaultValue: undefined, optional: true },
+    browserPath: { type: String, alias: 'b', defaultValue: undefined, optional: true },
+});
+
+export default function getConfig(name: string, defaultValue: any = null): any {
     return config[name] || defaultValue;
 }
