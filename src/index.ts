4ce63ace05656b7cca301a19bc6eee6bf53215e6
Gabriel Gonzalez <gajosu15@gmail.com>
2022-11-15 01:58:04 -0500

    add container to index

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,7 @@
-import { getHttpServer } from "./config/HttpServer";
+import container from './container';
 import Controller from "./Libs/Controller";
-import WebServer from "./Libs/WebServer";
-import WebSocket from "./Libs/WebSocket";
-import Whatsapp from "./Libs/Whatsapp";
 
-
-const webConfig = getHttpServer();
-
-const whatsapp = new Whatsapp();
-const webServer = new WebServer(webConfig.app);
-const webSocket = new WebSocket(webConfig.server);
+const { whatsapp, webServer, webSocket } = container;
 
 const controller = new Controller(whatsapp, webServer, webSocket);
 controller.start();
