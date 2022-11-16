3f190586ec89e4b3a951d4f5025cf1d3829ab2e2
Gabriel Gonzalez <gajosu15@gmail.com>
2022-11-16 17:08:52 -0500

    add controller test

---
+++ tests/controller.test.ts
@@ -0,0 +1,41 @@
+import Controller from '../src/Libs/Controller';
+
+const mockLogger = {
+    getCategoryLogger: jest.fn().mockImplementation(() => mockLogger),
+    info: jest.fn(),
+    error: jest.fn(),
+    warn: jest.fn(),
+    debug: jest.fn()
+}
+
+const mockWhatsapp = {
+    start: jest.fn(),
+    stop: jest.fn(),
+    getClient: jest.fn()
+}
+
+const mockWebServer = {
+    start: jest.fn(),
+    stop: jest.fn()
+}
+
+const mockWebSocket = {
+    start: jest.fn(),
+    stop: jest.fn()
+}
+
+describe('controller', () => {
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+
+    it('start services', () => {
+        const controller = new Controller(mockLogger, mockWhatsapp, mockWebServer, mockWebSocket);
+        controller.start();
+
+        expect(mockLogger.info).toBeCalledWith('Starting Controller');
+        expect(mockWhatsapp.start).toBeCalledTimes(1);
+        expect(mockWebServer.start).toBeCalledTimes(1);
+        expect(mockWebSocket.start).toBeCalledTimes(1);
+    });
+});
