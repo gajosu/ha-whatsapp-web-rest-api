c40e7d4bd01a81eb5a30674b1e32713c8d919f98
Gabriel Gonzalez <gajosu15@gmail.com>
2022-11-17 15:19:09 -0500

    add di to http controller with express

--- src/Libs/WebServer.ts
+++ src/Libs/WebServer.ts
@@ -1,9 +1,14 @@
 import express from 'express'
 import { ILogger } from './Logger'
+import createContext from 'express-async-context'
+import container from '../container'
+import api from '../routes/api'
 
 export interface IWebServer {
     start: () => void
 }
+
+const Context = createContext(() => container)
 
 export default class WebServer implements IWebServer {
     constructor (private readonly app: express.Application, private readonly logger: ILogger) {
@@ -11,10 +16,14 @@ export default class WebServer implements IWebServer {
     }
 
     public start (): void {
+        this.app.use(Context.provider)
+        this.app.use(express.json())
+
         this.setRoutes()
     }
 
     private setRoutes (): void {
+        this.app.use('/api', api(Context))
         this.app.get('/', (req, res) => {
             res.sendFile('resources/views/index.html', { root: './src/' })
         })
--- src/http/controllers/front.js
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from "express";
-
-exports.index = (req: Request, res: Response) => {
-    res.sendFile('resources/views/index.html', { root: './' });
-}
