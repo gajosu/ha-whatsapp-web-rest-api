ca49323ade88390989121a399e2dc389db821e5f
Gabriel Gonzalez <gajosu15@gmail.com>
2022-11-18 01:28:03 -0500

    add whatsapp status checker middleware

---
+++ src/http/middlewares/WhatsappStatusChecker.ts
@@ -0,0 +1,27 @@
+import { IWhatsapp } from './../../Libs/Whatsapp'
+import { ILogger } from './../../Libs/Logger'
+import Express from 'express'
+
+const statusChecker = (
+    req: Express.Request,
+    res: Express.Response,
+    next: any
+) => async ({ logger, whatsapp }: { logger: ILogger, whatsapp: IWhatsapp }) => {
+    try {
+        await whatsapp.getClient().getState().then((state) => {
+            if (state === 'CONNECTED') {
+                next()
+                return
+            }
+
+            res.status(503).json({
+                message: 'Whatsapp is not connected',
+                status: 503
+            })
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+export default statusChecker
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -2,9 +2,11 @@ import express from 'express'
 import { IServices } from '../container'
 import { ContextManager } from 'express-async-context/lib/types'
 import { store } from '../http/controllers/MessageController'
+import WhatsappStatusChecker from '../http/middlewares/WhatsappStatusChecker'
 
 export default function (context: ContextManager<IServices>): express.Router {
     const router = express.Router()
+    router.use(context.consumer(WhatsappStatusChecker))
     router.route('/messages')
         .post(
             context.consumer(store)
