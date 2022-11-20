6f37c0dbdad6bfce6e95faf35ece2c007667e375
Gabriel Gonzalez <gajosu15@gmail.com>
2022-11-20 14:04:19 -0500

    fix message validator, now it validates the "to" parameter, it must be a number or a chat id

--- src/http/validators/MessageValidator.ts
+++ src/http/validators/MessageValidator.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from 'express'
-import { check, body } from 'express-validator'
+import { check, body, oneOf } from 'express-validator'
 import Validator from './Validator'
 
 export default async function (request: Request, response: Response): Promise<void> {
-    await check('to').isNumeric().run(request)
+    await oneOf([
+        check('to').isNumeric(),
+        check('to').matches(/^[0-9]+@.+/)
+    ]).run(request)
     await check('msg')
         .if(body('url').not().exists())
         .isString()
