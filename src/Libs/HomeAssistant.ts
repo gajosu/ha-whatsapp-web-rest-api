8052179c397cb40f87f4bb46f08b000123373497
Gabriel Gonzalez <gajosu15@gmail.com>
2022-11-22 23:57:18 -0500

    add new events to ha module

--- src/Libs/HomeAssistant.ts
+++ src/Libs/HomeAssistant.ts
@@ -1,7 +1,7 @@
 import { IEventPublisher } from './../Services/HomeAssistant/EventPublisher'
 import { IEventBus } from './EventBus'
 import { ILogger } from './Logger'
-import { IMessageAckEvent, IMessageEvent, IStateChangeEvent } from './Whatsapp'
+import { IMessageAckEvent, IMessageEvent, IStateChangeEvent, IAuthFailureEvent, IMessageRevokeForEveryoneEvent, IMessageRevokeForMeEvent, IGroupNotificationEvent, ICallEvent } from './Whatsapp'
 
 export interface IHomeAssistant {
     start: () => void
@@ -24,12 +24,34 @@ export default class HomeAssistant implements IHomeAssistant {
 
         this.logger.info('Home Assistant integration enabled')
 
+        this.eventBus.register('whatsapp.authenticated', this.onAuthenticated.bind(this))
+        this.eventBus.register('whatsapp.auth_failure', this.onAuthFailure.bind(this))
+        this.eventBus.register('whatsapp.disconnected', this.onDisconnected.bind(this))
         this.eventBus.register('whatsapp.message', this.onMessage.bind(this))
         this.eventBus.register('whatsapp.message.create', this.onCreatedMessage.bind(this))
         this.eventBus.register('whatsapp.message.ack', this.onMessageAck.bind(this))
+        this.eventBus.register('whatsapp.message.revoke_for_everyone', this.onMessageRevokeForEveryone.bind(this))
+        this.eventBus.register('whatsapp.message.revoke_for_me', this.onMessageRevokeForMe.bind(this))
+        this.eventBus.register('whatsapp.group.join', this.onGroupJoin.bind(this))
+        this.eventBus.register('whatsapp.group.leave', this.onGroupLeave.bind(this))
+        this.eventBus.register('whatsapp.group.update', this.onGroupUpdate.bind(this))
+        this.eventBus.register('whatsapp.call', this.onCall.bind(this))
         this.eventBus.register('whatsapp.state', this.onChangedState.bind(this))
-        this.eventBus.register('whatsapp.authenticated', this.onAuthenticated.bind(this))
-        this.eventBus.register('whatsapp.disconnected', this.onDisconnected.bind(this))
+    }
+
+    private async onAuthenticated (): Promise<void> {
+        this.logger.info('onAuthenticated')
+        await this.sendToHomeAssistant('authenticated', {})
+    }
+
+    private async onAuthFailure (data: IAuthFailureEvent): Promise<void> {
+        this.logger.info('onAuthFailure')
+        await this.sendToHomeAssistant('auth_failure', data)
+    }
+
+    private async onDisconnected (): Promise<void> {
+        this.logger.info('onDisconnected')
+        await this.sendToHomeAssistant('disconnected', {})
     }
 
     private async onMessage (data: IMessageEvent): Promise<void> {
@@ -52,19 +74,44 @@ export default class HomeAssistant implements IHomeAssistant {
         await this.sendToHomeAssistant('message_ack', event)
     }
 
+    private async onMessageRevokeForEveryone (data: IMessageRevokeForEveryoneEvent): Promise<void> {
+        this.logger.info('onMessageRevokeForEveryone', data.message.id)
+        await this.sendToHomeAssistant('message_revoke_for_everyone', {
+            message: data.message.rawData,
+            revokedMessage: data.revokedMessage.rawData
+        })
+    }
+
+    private async onMessageRevokeForMe (data: IMessageRevokeForMeEvent): Promise<void> {
+        this.logger.info('onMessageRevokeForMe', data.message.id)
+        await this.sendToHomeAssistant('message_revoke_for_me', {
+            message: data.message.rawData
+        })
+    }
+
+    private async onGroupJoin (data: IGroupNotificationEvent): Promise<void> {
+        this.logger.info('onGroupJoin', data)
+        await this.sendToHomeAssistant('group_join', data)
+    }
+
+    private async onGroupLeave (data: IGroupNotificationEvent): Promise<void> {
+        this.logger.info('onGroupLeave', data)
+        await this.sendToHomeAssistant('group_leave', data)
+    }
+
+    private async onGroupUpdate (data: IGroupNotificationEvent): Promise<void> {
+        this.logger.info('onGroupUpdate', data)
+        await this.sendToHomeAssistant('group_update', data)
+    }
+
+    private async onCall (data: ICallEvent): Promise<void> {
+        this.logger.info('onCall', data)
+        await this.sendToHomeAssistant('call', data)
+    }
+
     private async onChangedState (data: IStateChangeEvent): Promise<void> {
         this.logger.info('onChangedState', data)
         await this.sendToHomeAssistant('state', data)
-    }
-
-    private async onAuthenticated (): Promise<void> {
-        this.logger.info('onAuthenticated')
-        await this.sendToHomeAssistant('authenticated', {})
-    }
-
-    private async onDisconnected (): Promise<void> {
-        this.logger.info('onDisconnected')
-        await this.sendToHomeAssistant('disconnected', {})
     }
 
     private async sendToHomeAssistant (event: string, data: any): Promise<void> {
--- tests/Libs/HomeAssistant.test.ts
+++ tests/Libs/HomeAssistant.test.ts
@@ -2,6 +2,12 @@ import mockLogger from '../stubs/Logger'
 import mockEventBus from '../stubs/EventBus'
 import mockEventPublisher from '../stubs/HaEventPublisher'
 import HomeAssistant from './../../src/Libs/HomeAssistant'
+import { findCallback } from '../utils/Utils'
+
+const mockStartHomeAssistant = (): void => {
+    const ha = new HomeAssistant(mockLogger, mockEventBus, mockEventPublisher)
+    ha.start()
+}
 
 describe('Home assistant', () => {
     afterEach(() => {
@@ -9,8 +15,7 @@ describe('Home assistant', () => {
     })
 
     it('not start ha service', async () => {
-        const ha = new HomeAssistant(mockLogger, mockEventBus, mockEventPublisher)
-        ha.start()
+        mockStartHomeAssistant()
 
         expect(mockLogger.warn).toHaveBeenCalledWith('Home Assistant integration disabled')
     })
@@ -18,25 +23,70 @@ describe('Home assistant', () => {
     it('start ha service', async () => {
         process.env.SUPERVISOR_TOKEN = 'token'
 
-        const ha = new HomeAssistant(mockLogger, mockEventBus, mockEventPublisher)
-        ha.start()
+        mockStartHomeAssistant()
 
         expect(mockLogger.info).toHaveBeenCalledWith('Home Assistant integration enabled')
+        expect(mockEventBus.register).toHaveBeenCalledWith('whatsapp.authenticated', expect.any(Function))
+        expect(mockEventBus.register).toHaveBeenCalledWith('whatsapp.auth_failure', expect.any(Function))
+        expect(mockEventBus.register).toHaveBeenCalledWith('whatsapp.disconnected', expect.any(Function))
         expect(mockEventBus.register).toHaveBeenCalledWith('whatsapp.message', expect.any(Function))
         expect(mockEventBus.register).toHaveBeenCalledWith('whatsapp.message.create', expect.any(Function))
         expect(mockEventBus.register).toHaveBeenCalledWith('whatsapp.message.ack', expect.any(Function))
+        expect(mockEventBus.register).toHaveBeenCalledWith('whatsapp.message.revoke_for_everyone', expect.any(Function))
+        expect(mockEventBus.register).toHaveBeenCalledWith('whatsapp.message.revoke_for_me', expect.any(Function))
+        expect(mockEventBus.register).toHaveBeenCalledWith('whatsapp.group.join', expect.any(Function))
+        expect(mockEventBus.register).toHaveBeenCalledWith('whatsapp.group.leave', expect.any(Function))
+        expect(mockEventBus.register).toHaveBeenCalledWith('whatsapp.group.update', expect.any(Function))
+        expect(mockEventBus.register).toHaveBeenCalledWith('whatsapp.call', expect.any(Function))
         expect(mockEventBus.register).toHaveBeenCalledWith('whatsapp.state', expect.any(Function))
-        expect(mockEventBus.register).toHaveBeenCalledWith('whatsapp.authenticated', expect.any(Function))
-        expect(mockEventBus.register).toHaveBeenCalledWith('whatsapp.disconnected', expect.any(Function))
+    })
+
+    it('onAuthenticated', async () => {
+        process.env.SUPERVISOR_TOKEN = 'token'
+
+        mockStartHomeAssistant()
+
+        const onAuthenticated = findCallback(mockEventBus.register.mock, 'whatsapp.authenticated')
+        await onAuthenticated('message')
+
+        expect(mockLogger.info).toHaveBeenCalledWith('onAuthenticated')
+        expect(mockEventPublisher.publish).toHaveBeenCalledWith('whatsapp_authenticated', {})
+    })
+
+    it('onAuthFailure', async () => {
+        process.env.SUPERVISOR_TOKEN = 'token'
+
+        mockStartHomeAssistant()
+
+        const event = {
+            message: 'message'
+        }
+
+        const onAuthFailure = findCallback(mockEventBus.register.mock, 'whatsapp.auth_failure')
+        await onAuthFailure(event)
+
+        expect(mockLogger.info).toHaveBeenCalledWith('onAuthFailure')
+        expect(mockEventPublisher.publish).toHaveBeenCalledWith('whatsapp_auth_failure', event)
+    })
+
+    it('onDisconnected', async () => {
+        process.env.SUPERVISOR_TOKEN = 'token'
+
+        mockStartHomeAssistant()
+
+        const onDisconnected = findCallback(mockEventBus.register.mock, 'whatsapp.disconnected')
+        await onDisconnected()
+
+        expect(mockLogger.info).toHaveBeenCalledWith('onDisconnected')
+        expect(mockEventPublisher.publish).toHaveBeenCalledWith('whatsapp_disconnected', {})
     })
 
     it('onMessage', async () => {
         process.env.SUPERVISOR_TOKEN = 'token'
 
-        const ha = new HomeAssistant(mockLogger, mockEventBus, mockEventPublisher)
-        ha.start()
+        mockStartHomeAssistant()
 
-        const onMessage = mockEventBus.register.mock.calls[0][1]
+        const onMessage = findCallback(mockEventBus.register.mock, 'whatsapp.message')
         await onMessage({ message: { id: 'id', rawData: { id: 'id' } } })
 
         expect(mockLogger.info).toHaveBeenCalledWith('onMessage', 'id')
@@ -46,10 +96,9 @@ describe('Home assistant', () => {
     it('onCreatedMessage', async () => {
         process.env.SUPERVISOR_TOKEN = 'token'
 
-        const ha = new HomeAssistant(mockLogger, mockEventBus, mockEventPublisher)
-        ha.start()
+        mockStartHomeAssistant()
 
-        const onCreatedMessage = mockEventBus.register.mock.calls[1][1]
+        const onCreatedMessage = findCallback(mockEventBus.register.mock, 'whatsapp.message.create')
         await onCreatedMessage({ message: { id: 'id', rawData: { id: 'id' } } })
 
         expect(mockLogger.info).toHaveBeenCalledWith('onCreatedMessage', 'id')
@@ -59,10 +108,9 @@ describe('Home assistant', () => {
     it('onMessageAck', async () => {
         process.env.SUPERVISOR_TOKEN = 'token'
 
-        const ha = new HomeAssistant(mockLogger, mockEventBus, mockEventPublisher)
-        ha.start()
+        mockStartHomeAssistant()
 
-        const onMessageAck = mockEventBus.register.mock.calls[2][1]
+        const onMessageAck = findCallback(mockEventBus.register.mock, 'whatsapp.message.ack')
         await onMessageAck({ message: { id: 'id', rawData: { id: 'id' } }, ack: 1 })
 
         const event = { message: { id: 'id' }, ack: 1 }
@@ -71,41 +119,116 @@ describe('Home assistant', () => {
         expect(mockEventPublisher.publish).toHaveBeenCalledWith('whatsapp_message_ack', event)
     })
 
+    it('onMessageRevokeForEveryone', async () => {
+        process.env.SUPERVISOR_TOKEN = 'token'
+
+        mockStartHomeAssistant()
+
+        const message = { id: 'id', rawData: { id: 'id' } }
+
+        const onMessageRevokeForEveryone = findCallback(mockEventBus.register.mock, 'whatsapp.message.revoke_for_everyone')
+        await onMessageRevokeForEveryone({ message, revokedMessage: message })
+
+        const event = { message: message.rawData, revokedMessage: message.rawData }
+
+        expect(mockLogger.info).toHaveBeenCalledWith('onMessageRevokeForEveryone', 'id')
+        expect(mockEventPublisher.publish).toHaveBeenCalledWith('whatsapp_message_revoke_for_everyone', event)
+    })
+
+    it('onMessageRevokeForMe', async () => {
+        process.env.SUPERVISOR_TOKEN = 'token'
+
+        mockStartHomeAssistant()
+
+        const message = { id: 'id', rawData: { id: 'id' } }
+
+        const onMessageRevokeForMe = findCallback(mockEventBus.register.mock, 'whatsapp.message.revoke_for_me')
+        await onMessageRevokeForMe({ message })
+
+        const event = { message: message.rawData }
+
+        expect(mockLogger.info).toHaveBeenCalledWith('onMessageRevokeForMe', 'id')
+        expect(mockEventPublisher.publish).toHaveBeenCalledWith('whatsapp_message_revoke_for_me', event)
+    })
+
+    it('onGroupJoin', async () => {
+        process.env.SUPERVISOR_TOKEN = 'token'
+
+        mockStartHomeAssistant()
+
+        const onGroupJoin = findCallback(mockEventBus.register.mock, 'whatsapp.group.join')
+
+        const event = {
+            group: {}
+        }
+
+        await onGroupJoin(event)
+
+        expect(mockLogger.info).toHaveBeenCalledWith('onGroupJoin', event)
+        expect(mockEventPublisher.publish).toHaveBeenCalledWith('whatsapp_group_join', event)
+    })
+
+    it('onGroupLeave', async () => {
+        process.env.SUPERVISOR_TOKEN = 'token'
+
+        mockStartHomeAssistant()
+
+        const onGroupLeave = findCallback(mockEventBus.register.mock, 'whatsapp.group.leave')
+
+        const event = {
+            group: {}
+        }
+
+        await onGroupLeave(event)
+
+        expect(mockLogger.info).toHaveBeenCalledWith('onGroupLeave', event)
+        expect(mockEventPublisher.publish).toHaveBeenCalledWith('whatsapp_group_leave', event)
+    })
+
+    it('onGroupUpdate', async () => {
+        process.env.SUPERVISOR_TOKEN = 'token'
+
+        mockStartHomeAssistant()
+
+        const onGroupUpdate = findCallback(mockEventBus.register.mock, 'whatsapp.group.update')
+
+        const event = {
+            group: {}
+        }
+
+        await onGroupUpdate(event)
+
+        expect(mockLogger.info).toHaveBeenCalledWith('onGroupUpdate', event)
+        expect(mockEventPublisher.publish).toHaveBeenCalledWith('whatsapp_group_update', event)
+    })
+
+    it('onCall', async () => {
+        process.env.SUPERVISOR_TOKEN = 'token'
+
+        mockStartHomeAssistant()
+
+        const onCall = findCallback(mockEventBus.register.mock, 'whatsapp.call')
+
+        const event = {
+            call: {}
+        }
+
+        await onCall(event)
+
+        expect(mockLogger.info).toHaveBeenCalledWith('onCall', event)
+        expect(mockEventPublisher.publish).toHaveBeenCalledWith('whatsapp_call', event)
+    })
+
     it('onState', async () => {
         process.env.SUPERVISOR_TOKEN = 'token'
 
-        const ha = new HomeAssistant(mockLogger, mockEventBus, mockEventPublisher)
-        ha.start()
+        mockStartHomeAssistant()
 
         const event = { state: 'state' }
-        const onState = mockEventBus.register.mock.calls[3][1]
+        const onState = findCallback(mockEventBus.register.mock, 'whatsapp.state')
         await onState(event)
 
         expect(mockLogger.info).toHaveBeenCalledWith('onChangedState', event)
         expect(mockEventPublisher.publish).toHaveBeenCalledWith('whatsapp_state', event)
-    })
-
-    it('onAuthenticated', async () => {
-        process.env.SUPERVISOR_TOKEN = 'token'
-
-        const ha = new HomeAssistant(mockLogger, mockEventBus, mockEventPublisher)
-        ha.start()
-        const onAuthenticated = mockEventBus.register.mock.calls[4][1]
-        await onAuthenticated()
-
-        expect(mockLogger.info).toHaveBeenCalledWith('onAuthenticated')
-        expect(mockEventPublisher.publish).toHaveBeenCalledWith('whatsapp_authenticated', {})
-    })
-
-    it('onDisconnected', async () => {
-        process.env.SUPERVISOR_TOKEN = 'token'
-
-        const ha = new HomeAssistant(mockLogger, mockEventBus, mockEventPublisher)
-        ha.start()
-        const onDisconnected = mockEventBus.register.mock.calls[5][1]
-        await onDisconnected()
-
-        expect(mockLogger.info).toHaveBeenCalledWith('onDisconnected')
-        expect(mockEventPublisher.publish).toHaveBeenCalledWith('whatsapp_disconnected', {})
     })
 })
