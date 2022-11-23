a924a2a718c021f99d1ea26f98861374102139ae
Gabriel Gonzalez <gajosu15@gmail.com>
2022-11-23 14:02:05 -0500

    fix utils

--- tests/utils/Utils.ts
+++ tests/utils/Utils.ts
@@ -1,4 +1,4 @@
 
-export function findCallback (mock: any, name: string): Function {
+export function findCallback (mock: any, name: string): () => void {
     return mock.calls.find((call: any) => call[0] === name)[1]
 }
