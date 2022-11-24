d67408c79879ef3dc5b9390bc41941ea54a8f41a
Gabriel Gonzalez <gajosu15@gmail.com>
2022-11-24 18:40:52 -0500

    add chat controller endpoints

--- src/http/controllers/ChatController.ts
+++ src/http/controllers/ChatController.ts
@@ -1,26 +1,73 @@
-import { IWhatsapp } from './../../Libs/Whatsapp'
+import { IChatGetter } from './../../Services/Chat/ChatGetter'
 import { Request, Response, NextFunction as Next } from 'express'
+import { IChatDeleter } from '../../Services/Chat/ChatDeleter'
+import { IChatArchive } from '../../Services/Chat/ChatArchive'
+import { IChatMute } from '../../Services/Chat/ChatMute'
+import { IChatPin } from '../../Services/Chat/ChatPin'
+import { IChatReader } from '../../Services/Chat/ChatReader'
+import { IChatStateSender } from '../../Services/Chat/ChatStateSender'
 
 export const index = (request: Request, response: Response, next: Next) =>
-    async ({ whatsapp }: { whatsapp: IWhatsapp }) => {
-        try {
-            const chats = await whatsapp.getClient().getChats()
+    async ({ chatGetter }: { chatGetter: IChatGetter }) =>
+        await chatGetter.all()
+            .then(chats => response.json(chats), next)
 
-            return response.json(chats)
-        } catch (err) {
-            next(err)
-        }
-    }
+export const archiveChat = (request: Request, response: Response, next: Next) =>
+    async ({ chatArchive }: { chatArchive: IChatArchive }) =>
+        await chatArchive.archive(request.params.id)
+            .then(() => response.status(204).send(), next)
+
+export const unarchiveChat = (request: Request, response: Response, next: Next) =>
+    async ({ chatArchive }: { chatArchive: IChatArchive }) =>
+        await chatArchive.unarchive(request.params.id)
+            .then(() => response.status(204).send(), next)
+
+export const muteChat = (request: Request, response: Response, next: Next) =>
+    async ({ chatMute }: { chatMute: IChatMute }) =>
+        await chatMute.mute(request.params.id)
+            .then(() => response.status(204).send(), next)
+
+export const unmuteChat = (request: Request, response: Response, next: Next) =>
+    async ({ chatMute }: { chatMute: IChatMute }) =>
+        await chatMute.unmute(request.params.id)
+            .then(() => response.status(204).send(), next)
+
+export const pinChat = (request: Request, response: Response, next: Next) =>
+    async ({ pinChat }: { pinChat: IChatPin }) =>
+        await pinChat.pin(request.params.id)
+            .then(() => response.status(204).send(), next)
+
+export const unpinChat = (request: Request, response: Response, next: Next) =>
+    async ({ pinChat }: { pinChat: IChatPin }) =>
+        await pinChat.unpin(request.params.id)
+            .then(() => response.status(204).send(), next)
+
+export const markChatAsRead = (request: Request, response: Response, next: Next) =>
+    async ({ chatReader }: { chatReader: IChatReader }) =>
+        await chatReader.read(request.params.id)
+            .then(() => response.status(204).send(), next)
+
+export const markChatAsUnread = (request: Request, response: Response, next: Next) =>
+    async ({ chatReader }: { chatReader: IChatReader }) =>
+        await chatReader.unread(request.params.id)
+            .then(() => response.status(204).send(), next)
+
+export const sendTypingState = (request: Request, response: Response, next: Next) =>
+    async ({ chatStateSender }: { chatStateSender: IChatStateSender }) =>
+        await chatStateSender.typing(request.params.id)
+            .then(() => response.status(204).send(), next)
+
+export const sendRecordingState = (request: Request, response: Response, next: Next) =>
+    async ({ chatStateSender }: { chatStateSender: IChatStateSender }) =>
+        await chatStateSender.recording(request.params.id)
+            .then(() => response.status(204).send(), next)
+
+export const sendClearState = (request: Request, response: Response, next: Next) =>
+    async ({ chatStateSender }: { chatStateSender: IChatStateSender }) =>
+        await chatStateSender.clear(request.params.id)
+            .then(() => response.status(204).send(), next)
 
 export const deleteChat = (request: Request, response: Response, next: Next) =>
-    async ({ whatsapp }: { whatsapp: IWhatsapp }) => {
-        try {
-            const { id } = request.params
-
-            await whatsapp.getClient().deleteChat(id)
-
-            return response.json({ message: 'Chat deleted' })
-        } catch (err) {
-            next(err)
-        }
-    }
+    async ({ chatDeleter }: { chatDeleter: IChatDeleter }) =>
+        await chatDeleter.delete(request.params.id)
+            .then(() => response.status(204).send(), next)
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import { IServices } from '../container'
 import { ContextManager } from 'express-async-context/lib/types'
 import { store } from '../http/controllers/MessageController'
+import * as ChatController from '../http/controllers/ChatController'
 import WhatsappStatusChecker from '../http/middlewares/WhatsappStatusChecker'
 
 export default function (context: ContextManager<IServices>): express.Router {
@@ -13,132 +14,132 @@ export default function (context: ContextManager<IServices>): express.Router {
     )
 
     router.route('/chats').get(
-        context.consumer(store)
+        context.consumer(ChatController.index)
     )
 
     router.route('/chats').delete(
-        context.consumer(store)
+        context.consumer(ChatController.deleteChat)
     )
 
     router.route('/chats/:id/archive').put(
-        context.consumer(store)
+        context.consumer(ChatController.archiveChat)
     )
 
     router.route('/chats/:id/unarchive').put(
-        context.consumer(store)
+        context.consumer(ChatController.unarchiveChat)
     )
 
     router.route('/chats/:id/mute').put(
-        context.consumer(store)
+        context.consumer(ChatController.muteChat)
     )
 
     router.route('/chats/:id/unmute').put(
-        context.consumer(store)
+        context.consumer(ChatController.unmuteChat)
     )
 
     router.route('/chats/:id/pin').put(
-        context.consumer(store)
+        context.consumer(ChatController.pinChat)
     )
 
     router.route('/chats/:id/unpin').put(
-        context.consumer(store)
+        context.consumer(ChatController.unpinChat)
     )
 
     router.route('/chats/:id/mark-as-read').put(
-        context.consumer(store)
+        context.consumer(ChatController.markChatAsRead)
     )
 
     router.route('/chats/:id/mark-as-unread').put(
-        context.consumer(store)
+        context.consumer(ChatController.markChatAsUnread)
     )
 
     router.route('/chats/:id/send-typing').put(
-        context.consumer(store)
+        context.consumer(ChatController.sendTypingState)
     )
 
     router.route('/chats/:id/send-recording').put(
-        context.consumer(store)
+        context.consumer(ChatController.sendRecordingState)
     )
 
     router.route('/chats/:id/clear-state').put(
-        context.consumer(store)
+        context.consumer(ChatController.sendClearState)
     )
 
-    router.route('/chats/:id/messages').get(
-        context.consumer(store)
-    )
+    // router.route('/chats/:id/messages').get(
+    //     context.consumer(store)
+    // )
 
-    router.route('/chats/:id/messages').delete(
-        context.consumer(store)
-    )
+    // router.route('/chats/:id/messages').delete(
+    //     context.consumer(store)
+    // )
 
-    router.route('/chats/groups').post(
-        context.consumer(store)
-    )
+    // router.route('/chats/groups').post(
+    //     context.consumer(store)
+    // )
 
-    router.route('/chats/groups/:id').put(
-        context.consumer(store)
-    )
+    // router.route('/chats/groups/:id').put(
+    //     context.consumer(store)
+    // )
 
-    router.route('/chats/groups/:id/invite-code').get(
-        context.consumer(store)
-    )
+    // router.route('/chats/groups/:id/invite-code').get(
+    //     context.consumer(store)
+    // )
 
-    router.route('/chats/groups/:id/invite-code/regenerate').put(
-        context.consumer(store)
-    )
+    // router.route('/chats/groups/:id/invite-code/regenerate').put(
+    //     context.consumer(store)
+    // )
 
-    router.route('/chats/groups/:id/leave').put(
-        context.consumer(store)
-    )
+    // router.route('/chats/groups/:id/leave').put(
+    //     context.consumer(store)
+    // )
 
-    router.route('/chats/groups/:id/participants').get(
-        context.consumer(store)
-    )
+    // router.route('/chats/groups/:id/participants').get(
+    //     context.consumer(store)
+    // )
 
-    router.route('/chats/groups/:id/participants').post(
-        context.consumer(store)
-    )
+    // router.route('/chats/groups/:id/participants').post(
+    //     context.consumer(store)
+    // )
 
-    router.route('/chats/groups/:id/participants').delete(
-        context.consumer(store)
-    )
+    // router.route('/chats/groups/:id/participants').delete(
+    //     context.consumer(store)
+    // )
 
-    router.route('/chats/groups/:id/participants/promote').put(
-        context.consumer(store)
-    )
+    // router.route('/chats/groups/:id/participants/promote').put(
+    //     context.consumer(store)
+    // )
 
-    router.route('/chats/groups/:id/participants/demote').put(
-        context.consumer(store)
-    )
+    // router.route('/chats/groups/:id/participants/demote').put(
+    //     context.consumer(store)
+    // )
 
-    router.route('/contacts').get(
-        context.consumer(store)
-    )
+    // router.route('/contacts').get(
+    //     context.consumer(store)
+    // )
 
-    router.route('/contacts/block').put(
-        context.consumer(store)
-    )
+    // router.route('/contacts/block').put(
+    //     context.consumer(store)
+    // )
 
-    router.route('/contacts/unblock').put(
-        context.consumer(store)
-    )
+    // router.route('/contacts/unblock').put(
+    //     context.consumer(store)
+    // )
 
-    router.route('/me/available').put(
-        context.consumer(store)
-    )
+    // router.route('/me/available').put(
+    //     context.consumer(store)
+    // )
 
-    router.route('/me/unavailable').put(
-        context.consumer(store)
-    )
+    // router.route('/me/unavailable').put(
+    //     context.consumer(store)
+    // )
 
-    router.route('/me/display-name').put(
-        context.consumer(store)
-    )
+    // router.route('/me/display-name').put(
+    //     context.consumer(store)
+    // )
 
-    router.route('/me/status').put(
-        context.consumer(store)
-    )
+    // router.route('/me/status').put(
+    //     context.consumer(store)
+    // )
 
     return router
 }
