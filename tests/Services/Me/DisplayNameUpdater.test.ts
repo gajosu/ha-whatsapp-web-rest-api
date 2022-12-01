5219dd985ffb28782c30ac3de31c4cdfd58a6b55
Gabriel Gonzalez <gajosu15@gmail.com>
2022-12-01 17:05:27 -0500

    fix code style

--- tests/Services/Me/DisplayNameUpdater.test.ts
+++ tests/Services/Me/DisplayNameUpdater.test.ts
@@ -2,7 +2,6 @@ import { mockWhatsappClient } from '../../stubs/WhatsappClient'
 import { mockWhatsapp } from '../../stubs/Whatsapp'
 import DisplayNameUpdater from '../../../src/Services/Me/DisplayNameUpdater'
 
-
 describe('Group chat finder service', () => {
     afterEach(() => {
         jest.clearAllMocks()
