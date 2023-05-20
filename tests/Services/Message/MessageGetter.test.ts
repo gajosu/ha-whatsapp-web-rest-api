fabf9fcc66b4bc03ac5cf32f86d940324a7ac1e7
Gabriel González <gajosu15@gmail.com>
2023-05-20 16:50:10 -0500

    Add message limit to MessageGetter service (#148)
    * Add limit to messages getter service
    
    * add docs
    
    * fix docs

--- tests/Services/Message/MessageGetter.test.ts
+++ tests/Services/Message/MessageGetter.test.ts
@@ -7,7 +7,7 @@ describe('get all messages', () => {
         jest.restoreAllMocks()
     })
 
-    it('react a message', async () => {
+    it('react a message with default limit', async () => {
         mockFinder.find.mockResolvedValue(mockChat)
         mockChat.fetchMessages.mockResolvedValue([mockChat])
 
@@ -15,7 +15,19 @@ describe('get all messages', () => {
         const messages = await service.all('chatId')
 
         expect(mockFinder.find).toBeCalledWith('chatId')
-        expect(mockChat.fetchMessages).toBeCalledWith({ limit: Infinity })
+        expect(mockChat.fetchMessages).toBeCalledWith({ limit: 100 })
+        expect(messages).toEqual([mockChat])
+    })
+
+    it('react a message with custom limit', async () => {
+        mockFinder.find.mockResolvedValue(mockChat)
+        mockChat.fetchMessages.mockResolvedValue([mockChat])
+
+        const service = new MessageGetter(mockFinder)
+        const messages = await service.all('chatId', 50)
+
+        expect(mockFinder.find).toBeCalledWith('chatId')
+        expect(mockChat.fetchMessages).toBeCalledWith({ limit: 50 })
         expect(messages).toEqual([mockChat])
     })
 })
