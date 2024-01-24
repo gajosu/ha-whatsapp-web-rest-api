ff4fa4c3ac74e7c116b6f272a02e589f231c6c17
Gabriel González <gajosu15@gmail.com>
2024-01-24 15:09:10 +0000

    fix ts syntax

--- src/Services/GroupChat/GroupChatCreator.ts
+++ src/Services/GroupChat/GroupChatCreator.ts
@@ -1,13 +1,13 @@
 import { CreateGroupResult } from 'whatsapp-web.js'
 import { IWhatsapp } from './../../Libs/Whatsapp'
 export interface IGroupChatCreator {
-    create: (name: string, participants: string[]) => Promise<CreateGroupResult>
+    create: (name: string, participants: string[]) => Promise<CreateGroupResult | string>
 }
 
 export default class GroupChatCreator implements IGroupChatCreator {
     public constructor (private readonly whatsapp: IWhatsapp) {}
 
-    public async create (name: string, participants: string[]): Promise<CreateGroupResult> {
+    public async create (name: string, participants: string[]): Promise<CreateGroupResult | string> {
         const client = this.whatsapp.getClient()
 
         const chat = await client.createGroup(name, participants)
